2778b241307bd8eb5006b29dc9af6941d9e2fcef
ValentinChb <valentin.chabaud@sintef.no>
2023-01-12 18:24:03 +0100

    debug yaw angle from file

--- src/YawAngleFromFile.f90
+++ src/YawAngleFromFile.f90
@@ -31,7 +31,7 @@ subroutine YawAngleFromFileSub(t,iT,dir_ctrl,y_out)
         do iT_loc=1,32
             if(y(iT_loc)/=0.0) y_prev(iT_loc)=y(iT_loc)
         enddo
-        read(fid,'(33E8.2)') t_loc, y
+        read(fid,*) t_loc, y
     enddo
 
     dt=t_loc-t_loc_prev
